d53971c8eb40e43593b5c4ce570ace0053c2503f
Jeffrey Vink <jcavink94@gmail.com>
2021-02-05 10:08:44 +0100

    Added defaults and import option

--- menu.py
+++ menu.py
@@ -1,26 +1,50 @@
-import airport
+from airport import *
+import_of_airports = True
+
 
 class Menu:
 	def __init__(self):
+		self.initialize()
+		self.main()
+		
+		
+	def main(self):
 		menu_item = input("What would you like to do?\n\t")
 		if menu_item == "list":
-			list()
+			self.list()
 		elif menu_item == "help":
-			help()
+			self.help()
 		elif menu_item == "exit":
-			_exit()
-
+			self._exit()
+		else:
+			self.invalid_menu_choice()
+	
+	
+	def invalid_menu_choice(self):
+		print("Invalid input, type 'help' to view valid options")
+		self.main()
+	
 		
 	def help(self):
 		print("Menu options:\n\thelp - brings up this menu")
 		print("\tlist - displays all airports")
-		main()
+		print("\texit - stops the application")
+		self.main()
 		
 
 	def list(self):
 		list_airports()
-		main()
+		self.main()
 		
 		
 	def _exit(self):
 		exit()
+		
+	
+	def initialize(self):
+		print("Import airports: "+str(import_of_airports))
+	
+		if import_of_airports:
+			res = requests.request("GET", url).text
+	
+		import_airports(res)
